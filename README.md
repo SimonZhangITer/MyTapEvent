ccac51789ac728368669e62df9e199e51ea581fb
张韧 <zhangr@ethicall.cn>
2016-12-27 10:36:14 +0800

    add param isStopPropagation

--- README.md
+++ README.md
@@ -20,55 +20,21 @@
 - 使用HTMLElement来扩充原型，方便添加Event
 - 使用单例模式，确保只加载一次
 
-ok，思想定下来，代码写起来就清晰多了：
-
-```javascript
-if (!HTMLElement.prototype.addTapEvent) {
-    HTMLElement.prototype.addTapEvent = function(callback) {
-        var tapStartTime = 0,
-            tapEndTime = 0,
-            tapTime = 500, //tap等待时间，在此事件下松开可触发方法
-            tapStartClientX = 0,
-            tapStartClientY = 0,
-            tapEndClientX = 0,
-            tapEndClientY = 0,
-            tapScollHeight = 15, //水平或垂直方向移动超过15px测判定为取消（根据chrome浏览器默认的判断取消点击的移动量)
-            cancleClick = false;
-        this.addEventListener('touchstart', function() {
-            tapStartTime = event.timeStamp;
-            var touch = event.changedTouches[0];
-            tapStartClientX = touch.clientX;
-            tapStartClientY = touch.clientY;
-            cancleClick = false;
-        })
-        this.addEventListener('touchmove', function() {
-            var touch = event.changedTouches[0];
-            tapEndClientX = touch.clientX;
-            tapEndClientY = touch.clientY;
-            if ((Math.abs(tapEndClientX - tapStartClientX) > tapScollHeight) || (Math.abs(tapEndClientY - tapStartClientY) > tapScollHeight)) {
-                cancleClick = true;
-            }
-        })
-        this.addEventListener('touchend', function() {
-            tapEndTime = event.timeStamp;
-            if (!cancleClick && (tapEndTime - tapStartTime) <= tapTime) {
-                callback();
-            }
-        })
-    }
-}
-```
 
 ##Usage
 
 ```javascript
 HTMLElement.addTapEvent(function(){
 	//do something...
-})
-如：
+},isStopPropagation)
+
+// isStopPropagation为是否阻止冒泡事件，默认为false
+
+// 使用方法如：
 document.querySelect('#test').addTapEvent(function(){
 	alert('this is a tap event');
-})
+},true)
+
 ```
 
 
--- js/myTapEvent.js
+++ js/myTapEvent.js
@@ -1,6 +1,6 @@
 (function() {
     if (!HTMLElement.prototype.addTapEvent) {
-        HTMLElement.prototype.addTapEvent = function(callback) {
+        HTMLElement.prototype.addTapEvent = function(callback, isStopPropagation) {
             var tapStartTime = 0,
                 tapEndTime = 0,
                 tapTime = 500, //tap等待时间，在此事件下松开可触发方法
@@ -11,13 +11,14 @@
                 tapScollHeight = 15, //水平或垂直方向移动超过15px测判定为取消（根据chrome浏览器默认的判断取消点击的移动量)
                 cancleClick = false;
             this.addEventListener('touchstart', function() {
+                var tempScroll = this.getAttribute('data-muse-component') == "html.system.Scroller" ? this : document.querySelector('[data-muse-component="html.system.Scroller"]');
                 tapStartTime = event.timeStamp;
                 var touch = event.changedTouches[0];
                 tapStartClientX = touch.clientX;
                 tapStartClientY = touch.clientY;
                 cancleClick = false;
             })
-            this.addEventListener('touchmove', function() {
+            this.addEventListener('touchmove', function(event) {
                 var touch = event.changedTouches[0];
                 tapEndClientX = touch.clientX;
                 tapEndClientY = touch.clientY;
@@ -25,7 +26,12 @@
                     cancleClick = true;
                 }
             })
-            this.addEventListener('touchend', function() {
+            this.addEventListener('touchend', function(event) {
+                if (isStopPropagation) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    event.stopImmediatePropagation();
+                }
                 tapEndTime = event.timeStamp;
                 if (!cancleClick && (tapEndTime - tapStartTime) <= tapTime) {
                     callback();
